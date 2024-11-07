f4bad4f806b60b7bd364839ab81d82404409c7b0
Nitr0-12 <ka4urin.nik@yandex.ru>
2024-11-07 23:31:44 +0300

    Update computer_store

--- shop/views.py
+++ shop/views.py
@@ -1,13 +1,34 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic.edit import CreateView
+from .models import Product, Purchase, PromoCode
 
-from .models import Product, Purchase
 
-# Create your views here.
 def index(request):
-    products = Product.objects.all()
-    context = {'products': products}
+    # Получаем все товары по умолчанию
+    all_products = Product.objects.all()
+
+    # Проверяем, был ли введен промокод
+    promo_code = request.GET.get('promo_code')
+    promo_products = []  # Список для товаров, добавляемых по промокоду
+    message = "Введите промокод, чтобы увидеть дополнительные товары."
+
+    # Если введен промокод, проверяем его наличие
+    if promo_code:
+        try:
+            promo = PromoCode.objects.get(code=promo_code)
+            # Получаем товары, привязанные к промокоду
+            promo_products = promo.products.all()
+            message = f"Промокод '{promo_code}' применен. Добавлены дополнительные товары."
+        except PromoCode.DoesNotExist:
+            message = "Неверный промокод."
+
+    context = {
+        'all_products': all_products,  # Все товары по умолчанию
+        'promo_products': promo_products,  # Товары, добавляемые по промокоду
+        'message': message,
+    }
+
     return render(request, 'shop/index.html', context)
 
 
@@ -18,4 +39,3 @@ class PurchaseCreate(CreateView):
     def form_valid(self, form):
         self.object = form.save()
         return HttpResponse(f'Спасибо за покупку, {self.object.person}!')
-
